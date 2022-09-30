e57e94b7a42667a9580c171201b56e57e74478df
g-rmc <g.rmc3000@gmail.com>
2022-09-30 14:06:20 -0300

    Route GET /games: query string implemented

--- src/controllers/games.controller.js
+++ src/controllers/games.controller.js
@@ -1,8 +1,14 @@
 import { connection } from '../db/database.js';
 
 async function getGames (req,res) {
+
+    let filter = '';
+    if (req.query.name) {
+        filter = req.query.name.toLowerCase();
+    }
+
     try {
-        const games = await connection.query('SELECT * FROM games;');
+        const games = await connection.query("SELECT * FROM games WHERE lower(name) LIKE $1;", [filter+'%']);
         res.send(games.rows);
     } catch (error) {
         res.status(500).send(error);
